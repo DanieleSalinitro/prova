1783bdd63019c21b618193f8371612f62ebdf951
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-03-06 14:20:54 +0100

    mod

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let number = 100;
-const number2= 200;
-console.log(number2);
-console.log(number);
+// - pari o dispari: chiedere all'utente se vuole pari o dispari e un numero intero compreso tra 1 e 9. Generare un numero casuale compreso tra 1 e 9, che sarà la giocata del computer. Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
+
+// function randomnumber(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+
+let
